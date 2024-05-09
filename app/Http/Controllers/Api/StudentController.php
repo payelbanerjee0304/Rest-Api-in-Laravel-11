485148a1b3a3d5b057f41dd4820741f93b9fa885
Payel Banerjee <payelbanerjee9319@gmail.com>
2024-05-09 17:37:54 +0530

    My Final Commit

---
+++ app/Http/Controllers/Api/StudentController.php
@@ -0,0 +1,144 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Models\Student;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Facades\DB;
+
+
+class StudentController extends Controller
+{
+    public function index(){
+        $students= Student::all();
+        if ($students->count() > 0) {
+            return response()->json([
+            'status' => 200,
+            'students' => $students
+            ], 200);
+        }else{
+            return response()->json([
+            'status'=> 404,
+            'message' => 'No Records Found'
+            ], 404);
+        }
+    }
+    public function store(Request $request)
+    {
+        $validator = Validator:: make ($request->all(), [
+        'name' => 'required|string|max:191',
+        'course' => 'required|string|max:191',
+        'email' => 'required|email| max:191',
+        'phone' => 'required| digits:10',
+        ]);
+        if($validator->fails()){
+            return response()->json([
+                'status' => 422,
+                'errors' => $validator->messages()
+            ],422);
+        }else{
+            $data=[
+                'name'=>$request->name,
+                'course'=>$request->course,
+                'email'=>$request->email,
+                'phone'=>$request->phone,
+            ];
+            $student = DB::table('students')->insert($data);
+            if($student){
+                return response()->json([
+                'status' => 200,
+                'message' => "Student Created Successfully"
+                ],200);
+            }else{
+                return response()->json([
+                'status' => 500,
+                'message' => "Something Went Wrong!"
+                ],500);
+            }
+        }
+    }
+    public function show($id){
+        
+        $student = Student::find($id);
+        if($student){
+            return response()->json([
+            'status' => 200,
+            'student' => $student
+            ],200);
+        }else{
+            return response()->json([
+            'status' => 404,
+            'message' => "No Such Student Found!"
+            ],404);
+        }
+    }
+    public function edit($id){
+
+        $student = Student::find($id);
+        if($student){
+            return response()->json([
+            'status' => 200,
+            'student' => $student
+            ],200);
+        }else{
+            return response()->json([
+            'status' => 404,
+            'message' => "No Such Student Found!"
+            ],404);
+        }
+    }
+    public function update(Request $request, int $id){
+
+        $validator = Validator:: make ($request->all(), [
+            'name' => 'required|string|max:191',
+            'course' => 'required|string|max:191',
+            'email' => 'required|email| max:191',
+            'phone' => 'required| digits:10',
+        ]);
+        if($validator->fails()){
+            return response()->json([
+                'status' => 422,
+                'errors' => $validator->messages()
+            ],422);
+        }else{
+            // $userid = Student::find($id);
+            $data=[
+                'name'=>$request->name,
+                'course'=>$request->course,
+                'email'=>$request->email,
+                'phone'=>$request->phone,
+            ];
+            $student = DB::table('students')->where('id','=',$id)->update($data);
+            if($student){
+                return response()->json([
+                'status' => 200,
+                'message' => "Student Updated Successfully"
+                ],200);
+            }else{
+                return response()->json([
+                'status' => 500,
+                'message' => "No Such Student Found!"
+                ],500);
+            }
+        }
+    }
+    public function destroy($id){
+
+        $student = Student::find($id);
+
+        if($student){
+            $student->delete();
+            return response()->json([
+                'status' => 200,
+                'message' => "Student Deleted Successfully"
+            ],200);
+        }else{
+            return response()->json([
+            'status' => 404,
+            'message' => "No Such Student Found!"
+            ],404);
+        }
+    }
+}
